dc215eb5acd5a9be0380cdc5ac751de244238b3c
Jeremy <nowak.jeremy.d@gmail.com>
2022-09-12 12:53:20 -0700

    Create array of calculator buttons

---
+++ script.js
@@ -0,0 +1,24 @@
+const buttonArray = [
+    {"buttonID": "zero", "symbol": "0"},
+    {"buttonID": "one", "symbol": "1"},
+    {"buttonID": "two", "symbol": "2"},
+    {"buttonID": "three", "symbol": "3"},
+    {"buttonID": "four", "symbol": "4"},
+    {"buttonID": "five", "symbol": "5"},
+    {"buttonID": "six", "symbol": "6"},
+    {"buttonID": "seven", "symbol": "7"},
+    {"buttonID": "eight", "symbol": "8"},
+    {"buttonID": "nine", "symbol": "9"},
+    {"buttonID": "add", "symbol": "+"},
+    {"buttonID": "subtract", "symbol": "-"},
+    {"buttonID": "multiply", "symbol": "X"},
+    {"buttonID": "divide", "symbol": "/"},
+    {"buttonID": "equal", "symbol": "="},
+    {"buttonID": "decimal", "symbol": "."}
+]
+
+
+function createCalcButtons () {
+    buttonArea = document.getElementById(calcButtons);
+
+}
